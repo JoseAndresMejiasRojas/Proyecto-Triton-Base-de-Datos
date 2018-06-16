ef38caa758562ec9e285697b6dad9c8cddd79582
Jose Mejias Rojas <b54275@ecci.ucr.ac.cr>
2018-06-15 23:19:34 -0600

    Se agregaron archivos para tener un registro de cómo insertar datos en la base.

---
+++ InsercionAtletas.sql
@@ -0,0 +1,14 @@
+Use Triton
+
+INSERT INTO Atleta
+values 
+('Ana@gmail.com'	,'L',11112	, 'Costa Rica'	, 'San Jose', 'Goicochea'			, 'Facebook'	, '1997-08-14','Ana'		,'Castro'		,'Zuñiga'	,'Q',88411761, 'Nada.'		,'Model A', 'Sony'	, 'Bailin10'	, 12),
+('Bruno@gmail.com'	,'XL',58112	, 'Costa Rica'	, 'San Jose', 'Montes de Oca'		, 'Instagram'	, '1998-03-19','Bruno'		,'Gomez'		,'Rodriguez','M',88854785, 'Ejercicio.'	,'Model B', 'HP'	, 'Contraseña'	, 12),
+('Carlos@gmail.com'	,'S',61612	, 'USA'			, 'New York', 'Manhattan'			, 'Facebook'	, '1991-03-15','Carlos'		,'Sequeira'		,'Moya'		,'M',84961387, 'Yeah.'		,'Model C', 'Sony'	, '12345'		, 01),
+('Daniel@gmail.com'	,'M',38912	, 'Costa Rica'	, 'San Jose', 'Goicochea'			, 'Facebook'	, '1992-06-29','Daniel'		,'Escamilla'	,'Angulo'	,'M',71139997, 'Ponle.'		,'Model C', 'HP'	, 'Daniel'		, 01),
+('Elka@gmail.com'	,'S',44412	, 'USA'			, 'Florida'	, 'Miami'				, 'Instagram'	, '1987-05-17','Elka'		,'Malinowsji'	,'Gajda'	,'F',86514732, 'Dale.'		,'Model C', 'Sony'	, 'Chinook'		, 12),
+('Fanny@gmail.com'	,'M',31719	, 'Costa Rica'	, 'Alajuela', 'Poas'				, 'Facebook'	, '1969-12-30','Fanny'		,'Perez'		,'Cassasola','F',77889966, 'Vamos.'		,'Model B', 'Dell'	, 'Password'	, 01),
+('Gabriela@gmail.com','XL',54513	, 'Panama'	, 'Capital'	, 'ciudadela'			, 'Snap'		, '2000-11-22','Gabriela'	,'Mora'			,'Villalon'	,'F',56564545, 'Meh.'		,'Model A', 'Sony'	, 'Hola123'		, 01),
+('Hector@gmail.com'	,'XS',64182	, 'USA'			, 'Ohio'	, 'Winchester'			, 'Snap'		, '1987-06-21','Hector'		,'Mejias'		,'Quesada'	,'M',88485588, 'Algo.'		,'Model A', 'HP'	, '1$f.?64'		, 12),
+('Isaac@gmail.com'	,'S',95142	, 'Panama'		, 'Capital'	, 'ciudadela'			, 'Snap'		, '1978-02-01','Isaac'		,'Arroyo'		,'Quiros'	,'T',88411754, 'No quiero.'	,'Model B', 'Dell'	, 'Proteo'		, 01)
+
---
+++ InsercionEntrenamientosIND.sql
@@ -0,0 +1,15 @@
+USE Triton
+
+INSERT INTO Entrenamiento_Individual
+VALUES 
+(11, 'Natación'			, 'Nadar Dorso'			, 'Experto'),
+(12, 'Natación'			, 'Nadar Mariposa'		, 'Facil'),
+(13, 'Natación'			, 'Nadar Aguas Abierta'	, 'Medio'),
+(21, 'Atletismo'			, 'Correr'				, 'Facil'),
+(22, 'Atletismo'			, 'Saltar Alto'			, 'Medio'),
+(23, 'Atletismo'			, 'Saltar Largo'		, 'Medio'),
+(31, 'Ciclismo'			, 'Pedalear'			, 'Facil'),
+(32, 'Ciclismo'			, 'Pedalear Más'		, 'Experto'),
+(41, 'Acondicionamiento'	, 'Planchas'			, 'Experto'),
+(42, 'Acondicionamiento'	, 'Push Ups'			, 'Facil')	
+
