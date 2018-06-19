b81bbdf78ad7d21e94d3b85cf8f15ef420cc71cf
Daniel Escamilla Leon paez <b52513@ecci.ucr.ac.cr>
2018-06-19 15:36:41 -0600

    Insercion de pruebas fisicas

---
+++ Insercion_PruebaFisica.sql
@@ -0,0 +1,24 @@
+USE Triton
+
+INSERT INTO Prueba_Fisica
+VALUES 
+('Ana@gmail.com'		,'2018-08-14', 'Cardiaca'	,'Regular'),
+('Ana@gmail.com'		,'2017-11-13', 'Fitness'	,'Bueno'	),
+('Bruno@gmail.com'		,'2018-03-16', 'Fitness'	,'mala'),
+('Bruno@gmail.com'		,'2018-08-22', 'Cardiaca'	,'Mala'),
+('Carlos@gmail.com'		,'2016-06-16', 'Cardiaca'	,'Regular'),
+('Carlos@gmail.com'		,'2017-01-04', 'Fitness'	,'malo'),
+('Daniel@gmail.com'		,'2018-12-12', 'Fitness'	,'bueno'),
+('Daniel@gmail.com'		,'2016-12-14', 'Cardiaca'	,'Regular'),
+('Elka@gmail.com'		,'2018-04-15', 'Cardiaca'	,'Excelente'),
+('Elka@gmail.com'		,'2018-06-22', 'Fitness'	,'Excelente'),
+('Fanny@gmail.com'		,'2018-08-31', 'Cardiaca'	,'Regular'),
+('Fanny@gmail.com'		,'2018-09-04', 'Fitness'	,'bueno'),
+('Gabriela@gmail.com'	,'2018-11-15', 'Cardiaca'	,'Regular'),
+('Gabriela@gmail.com'	,'2018-02-14', 'Fitness'	,'malo'),
+('Gabriela@gmail.com'	,'2018-04-26', 'Fitness'	,'bueno'),
+('Gabriela@gmail.com'	,'2018-10-24', 'Cardiaca'	,'excelente'),
+('Hector@gmail.com'		,'2018-03-10', 'Cardiaca'	,'Regular'),
+('Hector@gmail.com'		,'2018-06-01', 'Fitness'	,'Regular'),
+('Hector@gmail.com'		,'2018-07-12', 'Cardiaca'	,'Regular')
+
