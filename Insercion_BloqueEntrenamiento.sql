c5377b73aba922cac3768b27dd91ceba1c3b7e76
Daniel Escamilla Leon paez <b52513@ecci.ucr.ac.cr>
2018-06-19 16:28:56 -0600

    Insercion de Bloque Entrenamiento

---
+++ Insercion_BloqueEntrenamiento.sql
@@ -0,0 +1,22 @@
+use triton 
+
+insert into Bloque_Entrenamiento
+values
+('Ana@gmail.com'		,1	,1.0	,'Descripción','Formulario','Lunes'),
+('Ana@gmail.com'		,9	,3.0	,'Descripción','Formulario','Martes'),
+('Ana@gmail.com'		,5	,2.0	,'Descripción','Formulario','Martes'),
+('Ana@gmail.com'		,4	,0.5	,'Descripción','Formulario','Lunes'),
+('Bruno@gmail.com'		,2	,2.5	,'Descripción','Formulario','Jueves'),
+('Bruno@gmail.com'		,11	,4.5	,'Descripción','Formulario','Miercoles'),
+('Bruno@gmail.com'		,10	,5.5	,'Descripción','Formulario','Martes'),
+('Carlos@gmail.com'		,13	,3.5	,'Descripción','Formulario','Miercoles'),
+('Carlos@gmail.com'		,5	,1.5	,'Descripción','Formulario','Miercoles'),
+('Daniel@gmail.com'		,4	,0.5	,'Descripción','Formulario','Jueves'),
+('Elka@gmail.com'		,5	,2.0	,'Descripción','Formulario','Viernes'),
+('Elka@gmail.com'		,7	,1.0	,'Descripción','Formulario','Domingo'),
+('Elka@gmail.com'		,10	,5.5	,'Descripción','Formulario','Viernes'),
+('Elka@gmail.com'		,9	,3.0	,'Descripción','Formulario','Jueves'),
+('Fanny@gmail.com'		,6	,2.5	,'Descripción','Formulario','Sábado'),
+('Gabriela@gmail.com'	,7	,1.0	,'Descripción','Formulario','Domingo'),
+('Gabriela@gmail.com'	,5	,2.0	,'Descripción','Formulario','Lunes'),
+('Isaac@gmail.com'		,8	,2.5	,'Descripción','Formulario','Martes')
