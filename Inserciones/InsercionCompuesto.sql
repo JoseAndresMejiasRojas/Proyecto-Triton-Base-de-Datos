a4e81ffbb871772217c26d384a91cee23d867911
Daniel Escamilla Leon paez <b52513@ecci.ucr.ac.cr>
2018-06-20 17:53:07 -0600

    Fucking merge

---
+++ Inserciones/InsercionCompuesto.sql
@@ -0,0 +1,46 @@
+use Triton
+ 
+ insert into Compuesto
+ values 
+(11	,'Ana@gmail.com'		,1),--E1
+(23	,'Ana@gmail.com'		,4),--E4
+(41	,'Ana@gmail.com'		,5),--*
+(42	,'Ana@gmail.com'		,5),----*E5
+(32	,'Ana@gmail.com'		,5),---*
+(11	,'Ana@gmail.com'		,9),--*
+(12	,'Ana@gmail.com'		,9),----*E9
+(13	,'Ana@gmail.com'		,9),--*
+(12	,'Bruno@gmail.com'		,2),--*
+(22	,'Bruno@gmail.com'		,2),--*E2
+(31	,'Bruno@gmail.com'		,10),--*
+(42	,'Bruno@gmail.com'		,10),----*E10
+(41	,'Bruno@gmail.com'		,11),--*
+(42	,'Bruno@gmail.com'		,11),----*E11
+(41 ,'Carlos@gmail.com'		,5),--*
+(42	,'Carlos@gmail.com'		,5),----*E5
+(32	,'Carlos@gmail.com'		,5),--*
+(11, 'Carlos@gmail.com'		,13),--*
+(12, 'Carlos@gmail.com'		,13),--*E13
+(23	,'Daniel@gmail.com'		,4),--E4
+(41	,'Elka@gmail.com'		,5),--*
+(42	,'Elka@gmail.com'		,5),----*E5
+(32	,'Elka@gmail.com'		,5),--*
+(21	,'Elka@gmail.com'		,7),--*
+(31, 'Elka@gmail.com'		,7),--*E7
+(11, 'Elka@gmail.com'		,9),--*
+(12, 'Elka@gmail.com'		,9),----*E9
+(13, 'Elka@gmail.com'		,9),--*
+(31 ,'Elka@gmail.com'		,10),--*
+(42 ,'Elka@gmail.com'		,10),--*E10
+(13	,'Fanny@gmail.com'		,6),--*
+(31	,'Fanny@gmail.com'		,6),----*E6
+(41	,'Fanny@gmail.com'		,6),--*
+(41 ,'Gabriela@gmail.com'	,5),--*
+(42 ,'Gabriela@gmail.com'	,5),----*E5
+(32 ,'Gabriela@gmail.com'	,5),--*
+(21 ,'Gabriela@gmail.com'	,7),--*
+(31 ,'Gabriela@gmail.com'	,7),----*E7
+(42, 'Isaac@gmail.com'		,8)--E8
+
+
+
