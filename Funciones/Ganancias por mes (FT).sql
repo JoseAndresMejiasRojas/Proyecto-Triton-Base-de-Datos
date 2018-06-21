7701a04a0922f86d5ee3f11ab67fb0e838fb6e06
Jose Andrés <jose.andresmr@hotmail.com>
2018-06-21 17:57:33 -0600

    Se terminó la función que retorna las ganancias por mes.

--- Funciones/Ganancias por mes (FT).sql
+++ Funciones/Ganancias por mes (FT).sql
@@ -1,10 +1,37 @@
-CREATE FUNCTION dbo.Obtener_Gananancias_Mes(@Mes TINYINT )
+/*
+	NOTA: para poder ejecutarlo, se usa la siguiente instrucción:
+	SELECT dbo.Obtener_Ganancias_Mes(int)
+
+	COALESCE se usa porque SUM, si no hay tuplas, devuelve NULL.  Esto hace que si hay un NULL, el valor que escoja
+	sea un 0.
+*/
+
+CREATE FUNCTION Obtener_Ganancias_Mes(@Mes TINYINT )
 RETURNS INTEGER
 AS
 BEGIN
-	DECLARE @Ganancias TINYINT
-	
-	-- Falta los SELECT necesarios para obtener los montos finales de Cobro_Mensual y Cobro_Individual.
+	DECLARE @Ganancias INTEGER
 
-	RETURN @Ganancias
+	IF  EXISTS 	(
+	SELECT Cobro.ID_Factura_PK
+	FROM Cobro JOIN Cobro_Mensual
+	ON Cobro.ID_Factura_PK = Cobro_Mensual.ID_Factura_FK) 
+		SET @Ganancias =	(SELECT COALESCE(SUM(Cobro_Mensual.Monto_Final), 0)
+							FROM Cobro_Mensual	JOIN Cobro ON Cobro.ID_Factura_PK = Cobro_Mensual.ID_Factura_FK
+												JOIN Atleta ON Atleta.Correo_PK = Cobro.Correo_FK
+		WHERE MONTH(Cobro.Fecha_Pago) = @Mes
+
+		);
+
+	IF EXISTS (
+		SELECT Cobro.ID_Factura_PK
+		FROM Cobro JOIN Cobro_Individual
+		ON Cobro.ID_Factura_PK = Cobro_Individual.ID_Factura_FK) 
+			SET @Ganancias +=	(SELECT COALESCE(SUM(Cobro_Individual.Monto_Total), 0)
+								FROM Cobro_Individual	JOIN Cobro ON Cobro.ID_Factura_PK = Cobro_Individual.ID_Factura_FK
+														JOIN Atleta ON Atleta.Correo_PK = Cobro.Correo_FK
+		WHERE MONTH(Cobro.Fecha_Pago) = @Mes
+		);
+
+		RETURN @Ganancias
 END;
