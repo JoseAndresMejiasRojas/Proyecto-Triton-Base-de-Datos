5ac1ffe18617aa02ce0a58e267168797732f6075
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2018-06-22 17:47:58 -0600

    Arreglado el trigger para no usar INSTEAD OF FOR.

--- Triggers/Monto_Mensual (TR).sql
+++ Triggers/Monto_Mensual (TR).sql
@@ -1,9 +1,10 @@
 -- Al insertar una tupla, calculo el Monto_Final
-CREATE TRIGGER TR_Fecha_Finalizacion
-ON Cobro_Mensual INSTEAD OF INSERT
+CREATE TRIGGER TR_Monto_Final
+ON Cobro_Mensual AFTER INSERT
 AS
 	DECLARE @Monto_Final	INTEGER
 	DECLARE @Monto_Mensual	INTEGER
+	DECLARE @id_factura		INTEGER
 	DECLARE @Porcentaje		FLOAT
 
 	-- Obtengo el porcentaje.
@@ -11,7 +12,11 @@ AS
 	SELECT @Monto_Mensual = (SELECT Monto_Mensual FROM Inserted)
 	SELECT @Monto_Final = @Monto_Mensual*( 1 - (@Porcentaje / 100) )
 
-	-- Inserto la tupla con los datos de la inserción.
-	INSERT INTO Cobro_Mensual( ID_Factura_FK, Monto_Mensual, Monto_Final )
-		(SELECT i.ID_Factura_FK, i.Monto_Mensual, @Monto_Final
-		FROM Inserted i)
+	-- Obtengo el id_factura de la tupla recién insertada, esto no se puede hacer desde UPDATE.
+	SELECT @id_factura = ID_Factura_FK
+	FROM INSERTED
+
+	-- Modifico el Monto_Final para agregarle el posible descuento, a la tupla agregada.
+	UPDATE Cobro_Mensual
+	SET Monto_Final = @Monto_Final
+	WHERE ID_Factura_FK = @id_factura
