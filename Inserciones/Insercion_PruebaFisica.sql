03b8c14019ebf2aa108f1c2d5066b072d12a8345
Daniel Escamilla Leon paez <b52513@ecci.ucr.ac.cr>
2018-06-22 17:55:25 -0600

    insercion Pruebas Fisicas

--- Inserciones/Insercion_PruebaFisica.sql
+++ Inserciones/Insercion_PruebaFisica.sql
@@ -2,23 +2,24 @@ USE Triton
 
 INSERT INTO Prueba_Fisica
 VALUES 
-('Ana@gmail.com'		,'2018-08-14', 'Cardiaca'	,'Regular'),
-('Ana@gmail.com'		,'2017-11-13', 'Fitness'	,'Bueno'	),
-('Bruno@gmail.com'		,'2018-03-16', 'Fitness'	,'mala'),
-('Bruno@gmail.com'		,'2018-08-22', 'Cardiaca'	,'Mala'),
-('Carlos@gmail.com'		,'2016-06-16', 'Cardiaca'	,'Regular'),
-('Carlos@gmail.com'		,'2017-01-04', 'Fitness'	,'malo'),
-('Daniel@gmail.com'		,'2018-12-12', 'Fitness'	,'bueno'),
-('Daniel@gmail.com'		,'2016-12-14', 'Cardiaca'	,'Regular'),
-('Elka@gmail.com'		,'2018-04-15', 'Cardiaca'	,'Excelente'),
-('Elka@gmail.com'		,'2018-06-22', 'Fitness'	,'Excelente'),
-('Fanny@gmail.com'		,'2018-08-31', 'Cardiaca'	,'Regular'),
-('Fanny@gmail.com'		,'2018-09-04', 'Fitness'	,'bueno'),
-('Gabriela@gmail.com'	,'2018-11-15', 'Cardiaca'	,'Regular'),
-('Gabriela@gmail.com'	,'2018-02-14', 'Fitness'	,'malo'),
-('Gabriela@gmail.com'	,'2018-04-26', 'Fitness'	,'bueno'),
-('Gabriela@gmail.com'	,'2018-10-24', 'Cardiaca'	,'excelente'),
-('Hector@gmail.com'		,'2018-03-10', 'Cardiaca'	,'Regular'),
-('Hector@gmail.com'		,'2018-06-01', 'Fitness'	,'Regular'),
-('Hector@gmail.com'		,'2018-07-12', 'Cardiaca'	,'Regular')
+( dbo.ObtenerCodigo('Ana@gmail.com')		,'2018-08-14', 'Cardiaca'	,'Regular'),
+( dbo.ObtenerCodigo('Ana@gmail.com')		,'2017-11-13', 'Fitness'	,'Bueno'  ),
+( dbo.ObtenerCodigo('Bruno@gmail.com')		,'2018-03-16', 'Fitness'	,'mala'),
+( dbo.ObtenerCodigo('Bruno@gmail.com')		,'2018-08-22', 'Cardiaca'	,'Mala'),
+( dbo.ObtenerCodigo('Carlos@gmail.com')		,'2016-06-16', 'Cardiaca'	,'Regular'),
+( dbo.ObtenerCodigo('Carlos@gmail.com')		,'2017-01-04', 'Fitness'	,'malo'),
+( dbo.ObtenerCodigo('Daniel@gmail.com')		,'2018-12-12', 'Fitness'	,'bueno'),
+( dbo.ObtenerCodigo('Daniel@gmail.com')		,'2016-12-14', 'Cardiaca'	,'Regular'),
+( dbo.ObtenerCodigo('Elka@gmail.com')		,'2018-04-15', 'Cardiaca'	,'Excelente'),
+( dbo.ObtenerCodigo('Elka@gmail.com')		,'2018-06-22', 'Fitness'	,'Excelente'),
+( dbo.ObtenerCodigo('Fanny@gmail.com')		,'2018-08-31', 'Cardiaca'	,'Regular'),
+( dbo.ObtenerCodigo('Fanny@gmail.com')		,'2018-09-04', 'Fitness'	,'bueno'),
+( dbo.ObtenerCodigo('Gabriela@gmail.com')	,'2018-11-15', 'Cardiaca'	,'Regular'),
+( dbo.ObtenerCodigo('Gabriela@gmail.com')	,'2018-02-14', 'Fitness'	,'malo'),
+( dbo.ObtenerCodigo('Gabriela@gmail.com')	,'2018-04-26', 'Fitness'	,'bueno'),
+( dbo.ObtenerCodigo('Gabriela@gmail.com')	,'2018-10-24', 'Cardiaca'	,'excelente'),
+( dbo.ObtenerCodigo('Hector@gmail.com')		,'2018-03-10', 'Cardiaca'	,'Regular'),
+( dbo.ObtenerCodigo('Hector@gmail.com')		,'2018-06-01', 'Fitness'	,'Regular'),
+( dbo.ObtenerCodigo('Hector@gmail.com')		,'2018-07-12', 'Cardiaca'	,'Regular')
+
 
