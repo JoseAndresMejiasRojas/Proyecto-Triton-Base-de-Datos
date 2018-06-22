72f74d82cc39997a2497b69b42e179645a85fd06
Jose Andrés <jose.andresmr@hotmail.com>
2018-06-21 18:51:48 -0600

    Arreglada la creación de tablas.

--- Creación de tablas.sql
+++ Creación de tablas.sql
@@ -4,8 +4,9 @@ USE Triton
 
 
 CREATE TABLE Atleta
-( 
-	Correo_PK			VARCHAR(30),
+(
+	Codigo_Atleta_PK	INTEGER IDENTITY,	-- IDENTITY genera un valor único para cada tupla.  Por defecto inicia en 1. 
+	Correo				VARCHAR(30)	UNIQUE,
 	Talla_Camiseta		VARCHAR(30),
 	ZIP_Code			INTEGER,
 	País				VARCHAR(30),
@@ -22,22 +23,22 @@ CREATE TABLE Atleta
 	Modelo				VARCHAR(50),
 	Marca				VARCHAR(50),
 	Contraseña			VARCHAR(20)	NOT NULL,
-	Inscripcion			INTEGER		NOT NULL
+	Inscripcion			MONEY		NOT NULL
 
-	CONSTRAINT PK_Correo_Atleta PRIMARY KEY (Correo_PK),
+	CONSTRAINT PK_Codigo_Atleta PRIMARY KEY (Codigo_Atleta_PK),
 	CONSTRAINT CK_ZIP_Code_Atleta CHECK (ZIP_Code >= 10000 AND ZIP_CODE <= 99999),	-- 5 dígitos.
-	CONSTRAINT CK_Inscripcion_Atleta CHECK (Inscripcion > 0)	
+	CONSTRAINT CK_Inscripcion_Atleta CHECK (Inscripcion >= 0)	
 );
 
 CREATE TABLE Prueba_Fisica
 ( 
-	Correo_FK		VARCHAR(30),
-	Fecha_Prueba_PK	DATE,
-	Tipo_Prueba_PK	VARCHAR(50),
-	Resultados		VARCHAR(500) NOT NULL	-- Si se crea la tupla, obligatoriamente se hizo una prueba, por lo que tiene que haber resultados.
+	Codigo_Atleta_FK	INTEGER,
+	Fecha_Prueba_PK		DATE,
+	Tipo_Prueba_PK		VARCHAR(50),
+	Resultados			VARCHAR(500) NOT NULL	-- Si se crea la tupla, obligatoriamente se hizo una prueba, por lo que tiene que haber resultados.
 
-	CONSTRAINT PK_Correo_Fecha_Prueba_Tipo_Prueba_Prueba_Fisica PRIMARY KEY (Correo_FK, Fecha_Prueba_PK, Tipo_Prueba_PK ),
-	CONSTRAINT FK_Correo_Prueba_Fisica_Prueba_Fisica FOREIGN KEY (Correo_FK)	REFERENCES Atleta(Correo_PK)
+	CONSTRAINT PK_Codigo_Fecha_Prueba_Tipo_Prueba_Prueba_Fisica PRIMARY KEY (Codigo_Atleta_FK, Fecha_Prueba_PK, Tipo_Prueba_PK ),
+	CONSTRAINT FK_Codigo_Prueba_Fisica_Prueba_Fisica FOREIGN KEY (Codigo_Atleta_FK)	REFERENCES Atleta(Codigo_Atleta_PK)
 		ON DELETE	CASCADE	-- Si se elimina el atleta, no hay porqué mantener las pruebas.
 		ON UPDATE	CASCADE
 );
@@ -45,7 +46,7 @@ CREATE TABLE Prueba_Fisica
 CREATE TABLE Cobro
 ( 
 	ID_Factura_PK			INTEGER,
-	Correo_FK				VARCHAR(30) DEFAULT 'Atleta suprimido',
+	Codigo_Atleta_FK		INTEGER DEFAULT -1,
 	Fecha_Pago				DATE		NOT NULL,
 	Fecha_Finalización		DATE		NOT NULL,
 	Número_Tarjeta			VARCHAR(16)	NOT NULL,
@@ -56,7 +57,7 @@ CREATE TABLE Cobro
 	Porcentaje_Descuento	INTEGER DEFAULT 0	-- CREO JEJE
 
 	CONSTRAINT PK_ID_Factura_Cobro PRIMARY KEY (ID_Factura_PK),
-	CONSTRAINT FK_Correo_Cobro FOREIGN KEY (Correo_FK) REFERENCES Atleta(Correo_PK)
+	CONSTRAINT FK_Correo_Cobro FOREIGN KEY (Codigo_Atleta_FK) REFERENCES Atleta(Codigo_Atleta_PK)
 		ON DELETE SET DEFAULT
 		ON UPDATE CASCADE,
 	CONSTRAINT CK_CVC_Cobro CHECK( CVC <= 999 AND CVC >= 0 ),
@@ -65,23 +66,23 @@ CREATE TABLE Cobro
 
 CREATE TABLE Cobro_Mensual
 (
-	ID_Factura_FK		INTEGER,
-	Monto_Mensual	INTEGER	NOT NULL,
-	Monto_Final		INTEGER NOT NULL
+	ID_Factura_FK	INTEGER,
+	Monto_Mensual	MONEY	NOT NULL,
+	Monto_Final		MONEY	NOT NULL
 
 	CONSTRAINT PK_ID_Factura_Cobro_Mensual PRIMARY KEY (ID_Factura_FK),
 	CONSTRAINT FK_ID_Factura_Cobro_Mensual FOREIGN KEY(ID_Factura_FK) REFERENCES Cobro(ID_Factura_PK)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE,
-	CONSTRAINT CK_Monto_Mensual_Cobro_Mensual CHECK( Monto_Mensual > 0 ),
-	CONSTRAINT CK_Monto_Final_Cobro_Mensual CHECK( Monto_Final > 0 )
+	CONSTRAINT CK_Monto_Mensual_Cobro_Mensual CHECK( Monto_Mensual >= 0 ),
+	CONSTRAINT CK_Monto_Final_Cobro_Mensual CHECK( Monto_Final >= 0 )
 );
 
 CREATE TABLE Cobro_Individual
 (
 	ID_Factura_FK		INTEGER,
-	Monto_Semanal		INTEGER	NOT NULL,
-	Monto_Total			INTEGER NOT NULL,
+	Monto_Semanal		MONEY	NOT NULL,
+	Monto_Total			MONEY	NOT NULL,
 	Cantidad_Semanas	INTEGER NOT NULL,
 
 	CONSTRAINT PK_ID_Factura_Cobro_Individual PRIMARY KEY (ID_Factura_FK),
@@ -95,31 +96,19 @@ CREATE TABLE Cobro_Individual
 
 CREATE TABLE Bloque_Entrenamiento
 (
-	Correo_FK				VARCHAR(30),
+	Codigo_Atleta_FK		INTEGER,
 	Numero_Entrenamiento_PK	INTEGER,
 	Rango_Horas				FLOAT			NOT NULL,
 	Descripcion				VARCHAR(500),
 	Formulario				VARCHAR(500),
 	Dia_Inicio				VARCHAR(10)		NOT NULL,
 
-	CONSTRAINT PK_Correo_Numero_Entrenamiento_Bloque_Entrenamiento PRIMARY KEY (Correo_FK, Numero_Entrenamiento_PK),
-	CONSTRAINT FK_Correo_Bloque_Entrenamiento FOREIGN KEY(Correo_FK) REFERENCES Atleta(Correo_PK)
+	CONSTRAINT PK_Codigo_Numero_Entrenamiento_Bloque_Entrenamiento PRIMARY KEY (Codigo_Atleta_FK, Numero_Entrenamiento_PK),
+	CONSTRAINT FK_Codigo_Bloque_Entrenamiento FOREIGN KEY(Codigo_Atleta_FK) REFERENCES Atleta(Codigo_Atleta_PK)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE,
 	CONSTRAINT CK_Rango_Horas_Bloque_Entrenamiento CHECK (Rango_Horas < 24.0 AND Rango_Horas > 0.0)
 
-);
-
-CREATE TABLE Etiqueta
-(
-	Correo_FK				VARCHAR(30),
-	Numero_Entrenamiento_FK	INTEGER,
-	Etiqueta_PK				VARCHAR(50)
-
-	CONSTRAINT PK_Correo_Numero_Entrenamiento_Etiqueta PRIMARY KEY (Correo_FK, Numero_Entrenamiento_FK, Etiqueta_PK), 
-	CONSTRAINT FK_Correo_Numero_Entrenamiento_Etiqueta FOREIGN KEY (Correo_FK, Numero_Entrenamiento_FK) REFERENCES Bloque_Entrenamiento(Correo_FK, Numero_Entrenamiento_PK)
-		ON DELETE CASCADE	-- OJO
-		ON UPDATE CASCADE
 );
 
 CREATE TABLE Entrenamiento_Individual
@@ -135,14 +124,26 @@ CREATE TABLE Entrenamiento_Individual
 CREATE TABLE Compuesto
 (
 	Codigo_Entrenamiento_FK	INTEGER,
-	Correo_FK				VARCHAR(30),
+	Codigo_Atleta_FK		INTEGER,
 	Numero_Entrenamiento_FK	INTEGER
 
-	CONSTRAINT PK_Codigo_Entrenamiento_Correo_Numero_Entrenamiento_Compuesto PRIMARY KEY (Codigo_Entrenamiento_FK, Correo_FK, Numero_Entrenamiento_FK),
-	CONSTRAINT FK_Correo_Numero_Entrenamiento_Compuesto FOREIGN KEY (Correo_FK, Numero_Entrenamiento_FK) REFERENCES Bloque_Entrenamiento(Correo_FK, Numero_Entrenamiento_PK)
+	CONSTRAINT PK_Codigo_Entrenamiento_Codigo_Atleta_Numero_Entrenamiento_Compuesto PRIMARY KEY (Codigo_Entrenamiento_FK, Codigo_Atleta_FK, Numero_Entrenamiento_FK),
+	CONSTRAINT FK_Codigo_Numero_Entrenamiento_Compuesto FOREIGN KEY (Codigo_Atleta_FK, Numero_Entrenamiento_FK) REFERENCES Bloque_Entrenamiento(Codigo_Atleta_FK, Numero_Entrenamiento_PK)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE,
 	CONSTRAINT FK_Codigo_Entrenamiento_Compuesto FOREIGN KEY (Codigo_Entrenamiento_FK) REFERENCES Entrenamiento_Individual(Codigo_Entrenamiento_PK)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE
 );
+
+CREATE TABLE Etiqueta
+(
+	Etiqueta_PK				VARCHAR(50),
+	Codigo_Entrenamiento_FK	INTEGER
+
+	CONSTRAINT PK_Correo_Numero_Entrenamiento_Etiqueta PRIMARY KEY (Etiqueta_PK, Codigo_Entrenamiento_FK), 
+	CONSTRAINT FK_Etiqueta_Codigo_Entrenamiento FOREIGN KEY (Codigo_Entrenamiento_FK) REFERENCES Entrenamiento_Individual(Codigo_Entrenamiento_PK)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+);
+
