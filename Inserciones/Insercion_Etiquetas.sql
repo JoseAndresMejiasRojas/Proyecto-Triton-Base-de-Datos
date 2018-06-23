0295bb0d890257e390a5f45dbb903f88ef0dc9bb
Daniel Escamilla Leon paez <b52513@ecci.ucr.ac.cr>
2018-06-22 18:13:05 -0600

    insercion etiqueta

--- Inserciones/Insercion_Etiquetas.sql
+++ Inserciones/Insercion_Etiquetas.sql
@@ -2,10 +2,11 @@ USE triton
 
 INSERT INTO Etiqueta
 VALUES 
-('Ana@gmail.com'		,1, 'Iron Man'),
-('Elka@gmail.com'		,5, 'Polonia'),
-('Elka@gmail.com'		,7, 'Iron Man'),
-('Fanny@gmail.com'		,6,  'Indoor'),
-('Gabriela@gmail.com'	,5, 'Indoor'),
-('Isaac@gmail.com'		,8, 'Polonia')
+('Iron Man',11),
+('Polonia',12),
+('Iron Man',22),
+('Indoor',41),
+('Indoor',42),
+('Polonia',13)
+
 
