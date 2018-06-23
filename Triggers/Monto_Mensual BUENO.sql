679b7fb7d98a4bf261abc76c506f74a3ca43f1db
Jose Mejias Rojas <b54275@ecci.ucr.ac.cr>
2018-06-23 15:47:03 -0600

    Trigger arreglado: se usó cursor.

--- Triggers/Monto_Mensual BUENO.sql
+++ Triggers/Monto_Mensual BUENO.sql
@@ -2,28 +2,29 @@
 CREATE TRIGGER TR_Mensual_Monto_Final
 ON Cobro_Mensual AFTER INSERT
 AS
-	DECLARE @Monto_Final	INTEGER
-	DECLARE @Monto_Mensual	INTEGER
+	DECLARE @Monto_Final	MONEY
+	DECLARE @Monto_Mensual	MONEY
 	DECLARE @id_factura		INTEGER
 	DECLARE @Porcentaje		FLOAT
 
-	IF @@ROWCOUNT = 1 
-	BEGIN
-		SET @Porcentaje = (SELECT COALESCE(c.Porcentaje_Descuento, 0) FROM Inserted i	JOIN Cobro c ON c.ID_Factura_PK = i.ID_Factura_FK)
-		SET @Monto_Mensual = (SELECT i.Monto_Mensual FROM Inserted i JOIN Cobro_Mensual c ON c.ID_Factura_FK = i.ID_Factura_FK )
-		SET @Monto_Final = @Monto_Mensual*( 1 - (@Porcentaje / 100) )
+	DECLARE Cursor_Tupla_Individual CURSOR LOCAL FOR
+		SELECT c.Porcentaje_Descuento, i.Monto_Mensual, i.ID_Factura_FK
+		FROM INSERTED i JOIN Cobro c ON i.ID_Factura_FK = c.ID_Factura_PK;
 
-		-- Modifico el Monto_Final para agregarle el posible descuento, a la tupla agregada.
-		UPDATE Cobro_Mensual
-		SET Monto_Final = @Monto_Final
-		FROM INSERTED
-		WHERE Cobro_Mensual.ID_Factura_FK = INSERTED.ID_Factura_FK
-	END
-	ELSE
-	BEGIN
-		UPDATE Cobro_Mensual
-		SET Monto_Final = 5
-		FROM INSERTED
-		WHERE Cobro_Mensual.ID_Factura_FK IN
-			(SELECT INSERTED.ID_Factura_FK FROM INSERTED)
-	END
+		OPEN Cursor_Tupla_Individual
+		FETCH NEXT FROM Cursor_Tupla_Individual INTO @Porcentaje, @Monto_Mensual, @id_factura
+
+		WHILE (@@FETCH_STATUS <> -1)
+		BEGIN
+			
+			IF @Porcentaje IS NULL		-- Porcentaje puede ser nulo.
+				SET @Porcentaje = 0
+
+			SET @Monto_Final = @Monto_Mensual*( 1 - (@Porcentaje / 100) )
+
+			UPDATE Cobro_Mensual
+			SET Monto_Final = @Monto_Final
+			WHERE @id_factura = ID_Factura_FK
+
+			FETCH NEXT FROM Cursor_Tupla_Individual INTO @Porcentaje, @Monto_Mensual, @id_factura
+		END
